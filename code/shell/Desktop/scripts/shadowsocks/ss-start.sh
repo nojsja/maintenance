5b39754c606f16c6094d60308e292bd8c57cf770
NoJsJa <yangwei13@outlook.com>
2018-07-23 13:56:20 +0800

    update: shell scripts

---
+++ code/shell/Desktop/scripts/shadowsocks/ss-start.sh
@@ -0,0 +1,1 @@
+nohup sslocal -c shadowsocks.json &
--- code/shell/Desktop/startup.sh
+++ code/shell/Desktop/startup.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Shadowsocks
+# # Shadowsocks
 cd /home/nojsja/Desktop/scripts/shadowsocks/
+rm nohup.out -f
 sudo sh ss-start.sh
 
-# Tim
+# # Tim
 # cd /home/nojsja/Desktop/software/QQ
 # nohup ./TIM-x86_64.AppImage &
 
-# Wechat
+# # Wechat
 cd /home/nojsja/software/wechat/electronic-wechat-linux-x64/
+rm nohup.out -f
 nohup ./electronic-wechat &
 
-# battery check
+# # Battery check
 statusFile=/sys/class/power_supply/BAT0/status
 nowStatus=$(cat $statusFile)
 if [[ $nowStatus == 'Discharging' ]]; then
   echo 'Warning! Discharging!!'
-  crontab -u nojsja /home/nojsja/Desktop/scripts/battery/schedule
+  crontab -u nojsja /home/nojsja/github/maintenance/code/shell/Desktop/scripts/battery/schedule
 fi
